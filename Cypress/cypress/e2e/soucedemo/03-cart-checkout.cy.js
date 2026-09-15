164e84ce07676b020af740aae2b45b5fa7b88ed3
ne0t4r <tester.stefanovic@gmail.com>
2026-09-15 14:11:35 +0200

    01-04

--- Cypress/cypress/e2e/soucedemo/03-cart-checkout.cy.js
+++ Cypress/cypress/e2e/soucedemo/03-cart-checkout.cy.js
@@ -1,1 +1,61 @@
-//in progress
+describe('03 - Cart & Checkout End-to-End Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/');
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
+  });
+
+  it('1. Úspešné dokončenie celej objednávky (Happy Path)', () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+    cy.get('.shopping_cart_badge').should('have.text', '2');
+
+    cy.get('.shopping_cart_link').click();
+    cy.url().should('include', '/cart.html');
+    cy.get('.cart_item').should('have.length', 2);
+
+    cy.get('[data-test="checkout"]').click();
+    cy.url().should('include', '/checkout-step-one.html');
+
+    cy.get('[data-test="firstName"]').type('Ján');
+    cy.get('[data-test="lastName"]').type('Novák');
+    cy.get('[data-test="postalCode"]').type('04001');
+    cy.get('[data-test="continue"]').click();
+
+    cy.url().should('include', '/checkout-step-two.html');
+    cy.get('.cart_item').should('have.length', 2);
+
+    cy.get('.summary_subtotal_label').should('contain', '$39.98'); // 29.99 + 9.99
+    cy.get('.summary_tax_label').should('contain', '$3.20');
+    cy.get('.summary_total_label').should('contain', '$43.18');
+
+    cy.get('[data-test="finish"]').click();
+
+    cy.url().should('include', '/checkout-complete.html');
+    cy.get('.complete-header').should('have.text', 'Thank you for your order!');
+  });
+
+  it('2. Validácia chýbajúcich údajov v pokladni (Negative Test)', () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('.shopping_cart_link').click();
+    cy.get('[data-test="checkout"]').click();
+
+    cy.get('[data-test="continue"]').click();
+
+    cy.get('[data-test="error"]')
+      .should('be.visible')
+      .and('contain', 'Error: First Name is required');
+  });
+
+  it('3. Odobranie položky priamo z košíka', () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('.shopping_cart_link').click();
+
+    cy.get('[data-test="remove-sauce-labs-backpack"]').click();
+
+    cy.get('.cart_item').should('not.exist');
+  });
+
+});
--- Cypress/cypress/e2e/soucedemo/04-api-tests.cy.js
+++ Cypress/cypress/e2e/soucedemo/04-api-tests.cy.js
@@ -1,1 +1,67 @@
-// in progress
+describe('04 - API Testing & Network Interception', () => {
+
+  const apiBaseUrl = 'https://reqres.in/api';
+
+  context('1. Direct Backend API Testing (ReqRes REST API)', () => {
+
+    it('1. GET - Získanie zoznamu používateľov a validácia dát', () => {
+      cy.request('GET', `${apiBaseUrl}/users?page=2`).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('page', 2);
+        expect(response.body.data).to.be.an('array').that.is.not.empty;
+        
+        const firstUser = response.body.data[0];
+        expect(firstUser).to.have.property('email');
+        expect(firstUser).to.have.property('first_name');
+        expect(firstUser.email).to.include('@reqres.in');
+      });
+    });
+
+    it('2. POST - Vytvorenie nového používateľa (Create User)', () => {
+      const newUser = {
+        name: 'Ján Novák',
+        job: 'QA Engineer'
+      };
+
+      cy.request('POST', `${apiBaseUrl}/users`, newUser).then((response) => {
+        expect(response.status).to.eq(201);
+        expect(response.body.name).to.eq(newUser.name);
+        expect(response.body.job).to.eq(newUser.job);
+        expect(response.body).to.have.property('id');
+        expect(response.body).to.have.property('createdAt');
+      });
+    });
+
+    it('3. DELETE - Vymazanie používateľa', () => {
+      cy.request('DELETE', `${apiBaseUrl}/users/2`).then((response) => {
+        expect(response.status).to.eq(204);
+      });
+    });
+
+  });
+
+  context('2. API Interception & Mocking (ReqRes API)', () => {
+
+    it('4. Mocking API odpovede (Simulácia vlastných dát cez cy.intercept)', () => {
+      cy.intercept('GET', `${apiBaseUrl}/users/1`, {
+        statusCode: 200,
+        body: {
+          data: {
+            id: 1,
+            email: 'custom.qa@portfolio.com',
+            first_name: 'Peter',
+            last_name: 'Tester'
+          }
+        }
+      }).as('getMockedUser');
+
+      cy.window().then((win) => {
+        win.fetch(`${apiBaseUrl}/users/1`);
+      });
+
+      cy.wait('@getMockedUser').its('response.body.data.email').should('eq', 'custom.qa@portfolio.com');
+    });
+
+  });
+
+});
